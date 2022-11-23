7fed0db3988f4963b2072cadbdb2bb4a5cf5dcef
shc5529 <shc5529@naver.com>
2022-11-24 01:36:50 +0900

    fix: permission denied error django.log in production server
    1. /var/log/django와 django.log가 permission denied error 발생해서 일단 주석 처리
    2. error 원인 확인 및 수정후 주석 해제 필요

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -32,9 +33,7 @@ AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = get_secret('AWS_REGION')
 AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
 AWS_LOCATION = 'static'
 AWS_MEDIA_LOCATION = 'media'
 STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
@@ -46,85 +45,86 @@ AWS_S3_USE_SSL = True
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+# TODO: production에서 /var/log/django와 django.log가 permission denied 발생 확인 필요 수정후 주석 해제 필요
 # Django Logging
-LOG_FILE = '/var/log/django/django.log'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'logfile': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': LOG_FILE,
-            'when': "midnight",  # 매 자정마다
-            'backupCount': 31,
-            'formatter': 'standard',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-    },
-    # Loggers (where does the log come from)
-    'loggers': {
-        'repackager': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['console', 'logfile'],
-            'propagate': True,
-            'level': 'WARN',
-        },
-        'django.server': {
-            'handlers': ['console', 'logfile'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'django.db.backends': {
-            'handlers': ['console', 'logfile'],
-            'level': 'WARN',
-            'propagate': False,
-        },
-        '': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'gunicorn.error': {
-            'level': 'INFO',
-            'handlers': ['logfile'],
-            'propagate': True,
-        },
-        'gunicorn.access': {
-            'level': 'INFO',
-            'handlers': ['logfile'],
-            'propagate': False,
-        },
-        'django.request': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    }
-}
+# LOG_FILE = '/var/log/django/django.log'
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'standard': {
+#             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'null': {
+#             'level': 'DEBUG',
+#             'class': 'logging.NullHandler',
+#         },
+#         'logfile': {
+#             'level': 'DEBUG',
+#             'class': 'logging.handlers.TimedRotatingFileHandler',
+#             'filename': LOG_FILE,
+#             'when': "midnight",  # 매 자정마다
+#             'backupCount': 31,
+#             'formatter': 'standard',
+#         },
+#         'console': {
+#             'level': 'INFO',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'standard'
+#         },
+#     },
+#     # Loggers (where does the log come from)
+#     'loggers': {
+#         'repackager': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         'django': {
+#             'handlers': ['console', 'logfile'],
+#             'propagate': True,
+#             'level': 'WARN',
+#         },
+#         'django.server': {
+#             'handlers': ['console', 'logfile'],
+#             'propagate': True,
+#             'level': 'INFO',
+#         },
+#         'django.db.backends': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'WARN',
+#             'propagate': False,
+#         },
+#         '': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'DEBUG',
+#         },
+#         'raven': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#             'propagate': False,
+#         },
+#         'sentry.errors': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#             'propagate': False,
+#         },
+#         'gunicorn.error': {
+#             'level': 'INFO',
+#             'handlers': ['logfile'],
+#             'propagate': True,
+#         },
+#         'gunicorn.access': {
+#             'level': 'INFO',
+#             'handlers': ['logfile'],
+#             'propagate': False,
+#         },
+#         'django.request': {
+#             'handlers': ['logfile'],
+#             'level': 'ERROR',
+#             'propagate': False,
+#         },
+#     }
+# }
